394e7a49dcdbddb1c2c2f44c0a6c17eed903ab99
dubi du <100496683@alumnos.uc3m.es>
2024-02-28 15:41:10 +0100

    plot

--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 # Para 3 dados cogiendo los 2 mejores y con orden 11, 21, 22, 31, 32, 33...
 
 # generamos las probabilidades con un patrón random que he encontrado
@@ -5,9 +6,8 @@ caras = 6
 pdf = []
 for i in range(caras):
     for ii in range(i):
-        pdf.append(3 + 6 * ii)
-    pdf.append(1 + 3 * i)
-print(pdf)
+        pdf.append((3 + 6 * ii) / (caras**3))
+    pdf.append((1 + 3 * i) / (caras**3))
 
 # cum sum
 cdf = []
@@ -17,4 +17,14 @@ for i in range(len(pdf)):
     else:
         cdf.append(cdf[-1] + pdf[i])
 
-print(cdf)
+xaxis = []
+for i in range(caras + 1):
+    dado1 = ""
+    dado1 = dado1 + str(i) + ","
+    for ii in range(i):
+        dado2 = dado1
+        dado2 = dado2 + str(ii + 1) + " "
+        xaxis.append(dado2)
+
+plt.bar(xaxis, pdf)
+plt.show()
